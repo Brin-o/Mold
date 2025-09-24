792b88af4f5228741610e4f2d0851924b8e39453
Brin <49071768+Brin-o@users.noreply.github.com>
2025-09-24 12:30:43 +0200

    GUI rework started

--- Scripts/text_loader.gd
+++ Scripts/text_loader.gd
@@ -1,7 +1,11 @@
 extends RichTextLabel
 
 func _ready():
-	text = load_text()
+	text = ""
+	try_to_add_image()
+	text += load_text()
+	pass
+	
 
 func load_text(filename = "test"):
 	var path = "res://Texts/" + filename + ".txt"
@@ -9,7 +13,17 @@ func load_text(filename = "test"):
 	var file = FileAccess.open(path , FileAccess.READ)
 	var content = file.get_as_text()
 	return content
-	
+
+func try_to_add_image(filename = "test"):
+	var path = "res://Texts/" + filename + ".png"
+	var img_file = FileAccess.open(path , FileAccess.READ)
+	if img_file == null:
+		print("no image")
+	else:
+		print("Adding image at the front of the text!")
+		text += "[img]" + path + "[/img]" 
+		text += "\n"
+	pass
 	
 func _input(event):
 	if event.is_action_released("ui_accept"):
