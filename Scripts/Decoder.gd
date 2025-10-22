96ecbe0d998a464035d698e47876e39aa79ea4e6
Brin <49071768+Brin-o@users.noreply.github.com>
2025-10-22 21:00:09 +0200

    decoder test scene

--- Scripts/Decoder.gd
+++ Scripts/Decoder.gd
@@ -1,12 +1,39 @@
 extends RichTextLabel
 
 
+var decoder_step = 0
+
+
 @export_multiline var section1coded: Array[String] = [
 	"persistent low-frequency hum across hyphal network; metabolic load decreasing.",
 	"AGAIN persistent low-frequency hum across hyphal network; metabolic load decreasing."
 	]
 	
 @export_multiline var section1decoded: Array[String] = [
+	"persistent low-frequency hum across hyphal network; metabolic load decreasing."
+	]
+	
+@export_multiline var section2coded: Array[String] = [
+	"persistent low-frequency hum across hyphal network; metabolic load decreasing.",
+	"AGAIN persistent low-frequency hum across hyphal network; metabolic load decreasing."
+	]
+@export_multiline var section2decoded: Array[String] = [
+	"persistent low-frequency hum across hyphal network; metabolic load decreasing."
+	]
+	
+@export_multiline var section3coded: Array[String] = [
+	"persistent low-frequency hum across hyphal network; metabolic load decreasing.",
+	"AGAIN persistent low-frequency hum across hyphal network; metabolic load decreasing."
+	]
+@export_multiline var section3decoded: Array[String] = [
+	"persistent low-frequency hum across hyphal network; metabolic load decreasing."
+	]
+	
+@export_multiline var section4coded: Array[String] = [
+	"persistent low-frequency hum across hyphal network; metabolic load decreasing.",
+	"AGAIN persistent low-frequency hum across hyphal network; metabolic load decreasing."
+	]
+@export_multiline var section4decoded: Array[String] = [
 	"persistent low-frequency hum across hyphal network; metabolic load decreasing."
 	]
 
@@ -19,16 +46,27 @@ func _ready():
 	text = combined_string
 	
 
+func display_page(page, decoded := false):
+	var currentPage = get("section"+str(page)+"coded")
+	var combined_string = String("\n").join(currentPage)
+	if(decoded):
+		decode_whole_section(currentPage)
+	text = combined_string
+	pass
+
 
 func _process(delta: float) -> void:
+	decoder_step = int(%SpinBox.value)
 	if Input.is_action_just_released("ui_accept"):
-		decode_whole_section()
+		display_page(decoder_step)
 	pass
 	
-func decode_whole_section():
-	for i in range(section1coded.size()):
-		insert_decoded_msg(section1coded[i], section1decoded[i])
-		await get_tree().create_timer(0.3).timeout
+func decode_whole_section(section, wait=false):
+	
+	for i in range(section.size()):
+		insert_decoded_msg(section[i], section[i])
+		if(wait): 
+			await get_tree().create_timer(0.3).timeout
 	pass
 
 
